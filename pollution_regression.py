41ae57dd5547cf37df75a78592b05e52bd19aa64
Iamthetvghost <elenamay516@gmail.com>
2026-09-01 20:21:18 +0800

    Add files via upload

---
+++ pollution_regression.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 20:09:07 2026
+
+@author: Elena
+"""
+
+import pandas as pd
+from function_local_regression import local_regression
+
+
+data = pd.read_csv("pollution_cleaneddata.csv")
+
+x = data["POOR"].values
+y = data["MORT"].values
+
+k = 60
+x_0 = [10, 18, 25]
+
+pred, se = local_regression(y, x, k, x_0)
+
+for i in range(len(x_0)):
+    print("POOR =", x_0[i])
+    print("Prediction =", pred[i])
+    print("SE =", se[i])
