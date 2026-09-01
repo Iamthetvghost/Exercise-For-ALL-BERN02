19b47aeaef756624cd7c8cbfc3dda2323774cef7
Iamthetvghost <elenamay516@gmail.com>
2026-09-01 21:17:35 +0800

    Update pollution_regression.py

--- pollution_regression.py
+++ pollution_regression.py
@@ -5,6 +5,7 @@ Created on Tue Sep  18:09:07 2026
 @author: Elena
 """
 
+import numpy as np
 import pandas as pd
 from function_local_regression import local_regression
 
@@ -14,7 +15,36 @@ data = pd.read_csv("pollution_cleaneddata.csv")
 x = data["POOR"].values
 y = data["MORT"].values
 
-k = 10
+
+cv_errors = []
+
+for k in range(3, len(x)):
+    errors = []
+
+    for i in range(len(x)):
+        x_train = np.delete(x, i)
+        y_train = np.delete(y, i)
+
+        pred_i, _ = local_regression(
+            y_train,
+            x_train,
+            k,
+            [x[i]]
+        )
+
+        errors.append((y[i] - pred_i[0]) ** 2)
+
+    cv_error = np.mean(errors)
+    cv_errors.append(cv_error)
+
+
+best_k = np.argmin(cv_errors) + 3
+
+print("Best k =", best_k)
+print("Cross-validation error =", min(cv_errors))
+
+
+k = best_k
 x_0 = [10, 18, 25]
 
 pred, se = local_regression(y, x, k, x_0)
